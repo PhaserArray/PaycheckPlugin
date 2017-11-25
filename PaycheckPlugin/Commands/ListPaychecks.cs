543f3a693896bd1afd79bfc984c2720359772ca8
Phaser Array <phaserarrayksp@gmail.com>
2017-11-25 10:03:47 +0200

    Completed list paychecks

--- PaycheckPlugin/Commands/ListPaychecks.cs
+++ PaycheckPlugin/Commands/ListPaychecks.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 using Rocket.API;
+using Rocket.Unturned.Chat;
 
 namespace PhaserArray.PaycheckPlugin.Commands
 {
@@ -8,18 +9,21 @@ namespace PhaserArray.PaycheckPlugin.Commands
 	{
 		public AllowedCaller AllowedCaller => AllowedCaller.Both;
 		public string Name => "listpaychecks";
-		public string Help => "Placeholder"; // TODO
-		public string Syntax => "Placeholder"; // TODO
+		public string Help => "Lists all paychecks"; // TODO
+		public string Syntax => "";
 		public List<string> Aliases => new List<string> {"lpay", "lpays", "listpaycheck"};
-		public List<string> Permissions => new List<string>();
+		public List<string> Permissions => new List<string> {"paychecks.commands.view"};
 
 		public void Execute(IRocketPlayer caller, string[] command)
 		{
 			var response = new StringBuilder();
-			foreach (var paycheck in COLLECTION)
+			response.Append("Current paychecks: ");
+			foreach (var paycheck in PaycheckPlugin.Config.Paychecks)
 			{
-				
+				response.AppendFormat(" {0}({1}),", paycheck.Name, paycheck.Experience);
 			}
+			response.Remove(response.Length - 1, 1);
+			UnturnedChat.Say(caller, response.ToString());
 		}
 	}
 }
