5283738e952288b3d44171422fb0b63d5675a792
Phaser Array <phaserarrayksp@gmail.com>
2017-11-28 10:54:49 +0200

    Added FindBestMatchIndex and made FindBestMatch use it

--- PaycheckPlugin/Helpers/PaycheckHelper.cs
+++ PaycheckPlugin/Helpers/PaycheckHelper.cs
@@ -5,8 +5,25 @@ namespace PhaserArray.PaycheckPlugin.Helpers
 {
 	public class PaycheckHelper
 	{
+		/// <summary>
+		/// Finds the best matching paycheck. Searches based on names.
+		/// </summary>
+		/// <param name="search"></param>
+		/// <returns>Paycheck</returns>
 		[CanBeNull]
 		public static Paycheck FindBestMatch(string search)
+		{
+			var index = FindBestMatchIndex(search);
+			return index != null ? PaycheckPlugin.Config.Paychecks[index.Value] : null;
+		}
+
+		/// <summary>
+		/// Finds the best matching paycheck's index. Searches based on names.
+		/// </summary>
+		/// <param name="search"></param>
+		/// <returns>Paycheck Index</returns>
+		[CanBeNull]
+		public static int? FindBestMatchIndex(string search)
 		{
 			search = search.ToLower();
 
@@ -17,21 +34,25 @@ namespace PhaserArray.PaycheckPlugin.Helpers
 			}
 			if (PaycheckPlugin.Config.Paychecks.Count == 1)
 			{
-				return PaycheckPlugin.Config.Paychecks[0].Name.ToLower().Contains(search)
-					? PaycheckPlugin.Config.Paychecks[0]
-					: null;
+				if (PaycheckPlugin.Config.Paychecks[0].Name.ToLower().Contains(search))
+				{
+					return 0;
+				}
+				return null;
 			}
 
 			var bestMatchPercentage = 0f;
-			Paycheck bestMatch = null;
-			foreach (var paycheck in PaycheckPlugin.Config.Paychecks)
+			int? bestMatch = null;
+			for (var i = 0; i < PaycheckPlugin.Config.Paychecks.Count; i++)
 			{
+				var paycheck = PaycheckPlugin.Config.Paychecks[i];
+
 				if (!paycheck.Name.ToLower().Contains(search)) continue;
 
 				var matchPercentage = search.Length / paycheck.Name.Length;
 				if (!(matchPercentage > bestMatchPercentage)) continue;
 				bestMatchPercentage = matchPercentage;
-				bestMatch = paycheck;
+				bestMatch = i;
 			}
 			return bestMatch;
 		}
